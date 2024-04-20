34e3239a29c43dcc8da7a3c4ac7b592be3f42103
MNxMunno <bilolhuzayfa@gmmail.com>
2024-04-20 15:22:57 +0500

    change

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Products from "../products/Products";
 import Hero from "../hero/Hero";
+import Category from "../category/Category";
 
 const Main = () => {
   return (
     <main>
       <Hero />
+      <Category />
       <Products />
     </main>
   );
